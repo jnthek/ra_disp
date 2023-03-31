0fc8c55f6790c2d5bdd53dc398509d18909b51aa
VU3VWB <vu3vwb@gmail.com>
2023-03-31 14:59:46 +0800

    SVD updated, Add a script to make gif

---
+++ misc/figs/make_movie.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ffmpeg -f image2 -r 8 -pattern_type glob -i '*.png' -vcodec libx264 -crf 22 video.gif
+# convert -delay 20 -loop 0 *.png animation.gif
+
+ffmpeg -framerate 4 -pattern_type glob -i '*.png' -r 15 -vf scale=512:-1 out.gif 
