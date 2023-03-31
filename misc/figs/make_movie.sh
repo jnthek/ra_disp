31766e8d1986cfa442bc72fdae232355729c8722
VU3VWB <vu3vwb@gmail.com>
2023-03-31 15:02:06 +0800

    Script modify

--- misc/figs/make_movie.sh
+++ misc/figs/make_movie.sh
@@ -3,4 +3,4 @@
 # ffmpeg -f image2 -r 8 -pattern_type glob -i '*.png' -vcodec libx264 -crf 22 video.gif
 # convert -delay 20 -loop 0 *.png animation.gif
 
-ffmpeg -framerate 4 -pattern_type glob -i '*.png' -r 15 -vf scale=512:-1 out.gif 
+ffmpeg -framerate 10 -pattern_type glob -i '*.png' -r 15 -vf scale=512:-1 out.gif 
